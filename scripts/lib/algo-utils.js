28c83c73be152f66fa4d0c86ffe5dfd1c7c1312e
Ashley Davis <ashley@codecapers.com.au>
2022-06-08 16:34:13 +1000

    Moved various functions to the more general purpose code file `algo-utils.js`.

--- scripts/lib/algo-utils.js
+++ scripts/lib/algo-utils.js
@@ -135,6 +135,82 @@ async function signAndSubmitTransaction(algodClient, fromAccount, txn) {
     };
 }
 
+//
+// Computes a transaction id from a transaction in a block.
+//
+function computeTransactionId(gh, gen, stib) {
+    const t = stib.txn;
+
+    // Manually add gh/gen to construct a correct transaction object
+    t.gh = gh;
+    t.gen = gen;
+
+    const stxn = {
+        txn: algosdk.Transaction.from_obj_for_encoding(t),
+    };
+
+    if ("sig" in stib) {
+        stxn.sig = stib.sig;
+    }
+
+    if ("lsig" in stib) {
+        stxn.lsig = stib.lsig;
+    }
+
+    if ("msig" in stib) {
+        stxn.msig = stib.msig;
+    }
+
+    if ("sgnr" in stib) {
+        stxn.sgnr = stib.sgnr;
+    }
+
+    return stxn.txn.txID();
+}
+
+//
+// Find a transaction by id in the specified block.
+//
+async function findTransaction(algodClient, roundNumber, txnId) {
+    const block = await algodClient.block(roundNumber).do();
+
+    for (const stxn of block.block.txns) {
+        if (computeTransactionId(block.block.gh, block.block.gen, stxn) === txnId) {
+            return stxn;
+        }
+    }
+
+    return undefined;
+}
+
+//
+// Print a transaction for debugging.
+//
+async function dumpTransaction(algodClient, roundNumber, txnId) {
+    dumpData(await findTransaction(algodClient, roundNumber, txnId));
+}
+
+//
+// Reads the global state of applications.
+//
+// https://developer.algorand.org/docs/get-details/dapps/smart-contracts/frontend/apps/#read-state
+//
+async function readGlobalState(client, addr, appId) {
+    const output = {};
+    const accountInfoResponse = await client.accountInformation(addr).do();
+    for (let i = 0; i < accountInfoResponse["created-apps"].length; i++) {
+        if (accountInfoResponse["created-apps"][i].id == appId) {
+            for (let n = 0; n < accountInfoResponse["created-apps"][i]["params"]["global-state"].length; n++) {
+                const variable = accountInfoResponse["created-apps"][i]["params"]["global-state"][n];
+                const name = Buffer.from(variable.key, "base64").toString();
+                output[name] = variable.value;
+            }
+        }
+    }
+    return output;
+}
+
+
 module.exports = {
     deployApp,
     deleteApp,
@@ -143,4 +219,7 @@ module.exports = {
     compileProgram,
     submitTransaction,
     signAndSubmitTransaction,
+    findTransaction,
+    dumpTransaction,
+    readGlobalState,
 };

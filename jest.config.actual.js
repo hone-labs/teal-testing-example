8042d7757dc226bbe893af107fc213529ea565d8
Ashley Davis <ashley@codecapers.com.au>
2022-06-14 10:29:59 +1000

    Removed Jest configuration files that are no longer needed.

--- jest.config.actual.js
+++
@@ -1,9 +0,0 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
-export default {
-    // Test files to consider.
-    testRegex: "/src/test/actual",
-};
